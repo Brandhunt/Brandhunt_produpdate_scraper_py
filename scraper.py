98e6956c97637c862977dd1c51430a97c9c2972c
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2019-12-17 12:30:37 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,9 +1,54 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
+
+#  /|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\  
+# <   -  Brandhunt Product Update Scraper   -   >
+#  \|/\|/\|/\|/\|/\|/\|/\|/\|/\|/\|/\|/\|/\|/\|/
+
+
+# --- IMPORT SECTION --- #
+
+import scraperwiki
+import lxml.html
+import os
+import requests
+import json
+import base64
+
+import mysql.connector
+
+#   Connect to Wordpress Site via REST API and get all the proper URLs to be scraped!
+
+wp_username = os.environ['MORPH_WP_USERNAME']
+wp_password = os.environ['MORPH_WP_PASSWORD']
+wp_connectwp_url = os.environ['MORPH_WP_CONNECT_URL']
+
+token = base64.standard_b64encode(wp_username + ':' + wp_password)
+headers = {'Authorization': 'Basic ' + token}
+
+#r = requests.post(wp_connectwp_url, headers=headers, json=post)
+r = requests.get(wp_connectwp_url, headers=headers)
+#print('Your post is published on ' + json.loads(r.content)['link'])
+#print('Data found: ' + json.loads(r.content)['link'])
+#print('Data found: ' + r.json())
+pretty_json = json.loads(r.content)
+print (json.dumps(pretty_json, indent=2))
+
+#   Connect to database and get the needed information!
+
+wp_db_name = os.environ['MORPH_DB_NAME']
+wp_db_user = os.environ['MORPH_DB_USER']
+wp_db_password = os.environ['MORPH_DB_PASSWORD']
+wp_db_host = os.environ['MORPH_DB_HOST']
+
+wp_db_charset = os.environ['MORPH_DB_CHARSET']
+wp_db_collate = ''
+
+cnx = mysql.connector.connect(user=wp_db_user, 
+                              password=wp_db_password,
+                              host=wp_db_host,
+                              database=wp_db_name)
+cnx.close()
+
 # # Read in a page
 # html = scraperwiki.scrape("http://foo.com")
 #
@@ -22,3 +67,4 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
