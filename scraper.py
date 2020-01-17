ca00e4174308cc998aed9ece70a454a172f1f20e
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-01-17 10:42:38 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -120,7 +120,7 @@ def reltoabs(relurl, baseurl):
 # --> First, check if the database should be reset:
 
 if bool(os.environ['MORPH_RESET_DB']):
-    scraperwiki.sql.execute('TRUNCATE data')
+    scraperwiki.sql.execute('DELETE FROM data')
 
 # --> Connect to Wordpress Site via REST API and get all the proper URLs to be scraped!
 
