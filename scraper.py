a7284b359a26098c375d3f82a6a9fcf94eecb129
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-01-18 10:30:12 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -446,8 +446,8 @@ while jsonprods:
                                         else:
                                             product_sex = [(doesprodattrexist(jsonprodattr['pa_sex'], 'Male', 'pa_sex'), False),
                                                           (doesprodattrexist(jsonprodattr['pa_sex'], 'Female', 'pa_sex'), False)]
-                                    print('SEX VALUES:')
-                                    #print(i)
+                                        #print('SEX VALUES:')
+                                        #print(i)
                                     for sex in product_sex: print(sex)
                                     # --> Attempt scraping of product misc. elements:
                                     prodmisc_backup = productmisc_array[i].strip().decode('string_escape')
