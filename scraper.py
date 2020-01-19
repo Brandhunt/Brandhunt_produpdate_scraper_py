de91e6db6312693854e2d7f94d063d7e6780f9b8
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-01-19 20:21:15 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -336,8 +336,8 @@ while jsonprods:
                                 #for img in image_elements: print img
                                 #print('IMAGE URLS:')
                                 #for img in image_urls: print img
-                                print('VALID IMAGES:')
-                                for img in image_urls_valid: print img
+                                #print('VALID IMAGES:')
+                                #for img in image_urls_valid: print img
                             except:
                                 #print("Error when scraping images for product ID " + product['productid'] + ": " + sys.exc_info()[0] + " occured!")
                                 print(traceback.format_exc())
