363620ff8c658d35fc02e8353aca97d0dd9f6377
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-01-22 00:52:05 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -24,7 +24,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options 
 
-from urllib2 import HTTPError
+#from urllib2 import HTTPError
+from urllib.error import HTTPError
 
 try:
     from urllib.parse import urljoin
