77102d312f85993327efb4a1fdca8df9a004dbe3
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-01-31 15:55:01 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -222,17 +222,22 @@ while jsonprods:
                                        user_agent='Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36')
                             else:
                                 headers = {
+                                    'Accept':'text/html,application/xhtml+xm…ml;q=0.9,image/webp,*/*;q=0.8',
+                                    'Accept-Encoding':'gzip, deflate, br',
+                                    'Accept-Language':'sv-SE,sv;q=0.8,en-US;q=0.5,en;q=0.3',
+                                    'DNT':'1',
                                     'User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',
                                     'Referer' : product['url']
                                 }
                                 session = requests.session()
                                 scraper = cfscrape.create_scraper(sess=session, delay=10)
+                                html = scraper.get(product['url'], headers=headers).content
                                 #scraper = cfscrape.create_scraper(delay=10)
                                 #scraper = cfscrape.create_scraper()
-                                if proxies:
-                                    html = scraper.get(product['url'], headers=headers, proxies=proxies).content
-                                else:
-                                    html = scraper.get(product['url'], headers=headers).content
+                                ###if proxies:
+                                ###    html = scraper.get(product['url'], headers=headers, proxies=proxies).content
+                                ###else:
+                                ###    html = scraper.get(product['url'], headers=headers).content
                                 '''s = socks.socksocket()
                                 proxy_https = re.split(':', proxy_https)
                                 s.set_proxy(socks.SOCKS4, proxy_https[0], proxy_https[1])
