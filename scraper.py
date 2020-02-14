71eba03f46a986c56b4a54d79c08408cebcb1262
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-02-14 01:16:26 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -298,28 +298,31 @@ while jsonprods:
                         try:
                             #html = scraperwiki.scrape(product['url'])
                             #print(str(use_alt_scrape))
-                            if use_alt_scrape is True:
+                            if use_alt_scrape is False:
                                 html = scraperwiki.scrape(product['url'],\
+                                       proxies=proxies,\
                                        user_agent='Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36')
+                                #html = scraperwiki.scrape(product['url'],\
+                                #       user_agent='Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36')
                             else:
-                                # #session = requests.Session()
-                                # #if proxies:
-                                # #    session.proxies = proxies
-                                # #session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'})
-                                # #html = session.get(product['url']).content
+                                session = requests.Session()
+                                if proxies:
+                                    session.proxies = proxies
+                                session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'})
+                                html = session.get(product['url']).content
                                 ###session.auth = proxauth
                                 ###
-                                headers = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',\
-                                    'Accept-Encoding':'gzip, deflate, br',\
-                                    'Accept-Language':'sv-SE,sv;q=0.8,en-US;q=0.5,en;q=0.3',\
-                                    'DNT':'1',\
-                                    'User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',\
-                                    'Referer' : product['url']}
+                                ###headers = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',\
+                                ###    'Accept-Encoding':'gzip, deflate, br',\
+                                ###    'Accept-Language':'sv-SE,sv;q=0.8,en-US;q=0.5,en;q=0.3',\
+                                ###    'DNT':'1',\
+                                ###    'User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',\
+                                ###    'Referer' : product['url']}
                                 #headers = {'Accept-Language':'sv-SE,sv;q=0.8,en-US;q=0.5,en;q=0.3',\
                                 #    'User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',\
                                 #    'Referer' : product['url']}
-                                session = requests.session()
-                                scraper = cfscrape.create_scraper(sess=session)
+                                ###session = requests.session()
+                                ###scraper = cfscrape.create_scraper(sess=session)
                                 #scraper = cfscrape.create_scraper(sess=session, delay=10)
                                 #html = scraper.get(product['url'], headers=headers).content
                                 #scraper = cfscrape.create_scraper(delay=10)
