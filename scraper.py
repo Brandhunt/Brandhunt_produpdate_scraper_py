799bd7f810c99d500629616616c57cac6708980e
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-06-17 04:40:12 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -450,7 +450,7 @@ while jsonprods:
                                 #print('PRICEBEFORECONVERSION:' + price)
                                 #print('PRICE ELEMENTS:')
                                 #for p in price_elements: print p
-                                if not re.match('[0-9]', price): price = ''
+                                #if not re.match('[0-9]', price): price = ''
                                 price = converttocorrectprice(price, website['currencysymbol'])
                             else:
                                 price = price.replace(r'[^0-9,.]', '')
@@ -483,7 +483,7 @@ while jsonprods:
                                         salesprice = re.sub('\\' + website['pricedelimitertoignore'].strip() + '', '', salesprice)    
 
                                 if website['currencysymbol']:
-                                    if not re.match('[0-9]', salesprice): salesprice = ''
+                                    #if not re.match('[0-9]', salesprice): salesprice = ''
                                     salesprice = converttocorrectprice(salesprice, website['currencysymbol'])
                                 else:
                                     salesprice = salesprice.replace(r'[^0-9,.]', '')
@@ -710,7 +710,7 @@ while jsonprods:
                                         newprice = price + productmisc_array[i].strip()
                                         if website['currencysymbol']:
                                             newprice.upper()
-                                            if not re.match('[0-9]', newprice): newprice = ''
+                                            #if not re.match('[0-9]', newprice): newprice = ''
                                             newprice = converttocorrectprice(newprice, website['currencysymbol'])
                                         else:
                                             newprice = newprice.replace(r'[^0-9,.]', '')
@@ -721,7 +721,7 @@ while jsonprods:
                                             newprice = salesprice + productmisc_array[i].strip()
                                             if website['currencysymbol']:
                                                 newprice.upper()
-                                                if not re.match('[0-9]', newprice): newprice = ''
+                                                #if not re.match('[0-9]', newprice): newprice = ''
                                                 newprice = converttocorrectprice(newprice, website['currencysymbol'])
                                             else:
                                                 newprice = newprice.replace(r'[^0-9,.]', '')
@@ -810,7 +810,7 @@ while jsonprods:
                                                                 newprice = re.sub('\\' + delim.strip() + '', '', newprice)
                                                         else:
                                                             newprice = re.sub('\\' + website['pricedelimitertoignore'].strip() + '', '', newprice)
-                                                    if not re.match('[0-9]', newprice): newprice = ''
+                                                    #if not re.match('[0-9]', newprice): newprice = ''
                                                     newprice = converttocorrectprice(newprice, website['currencysymbol'])
                                                 else:
                                                     newprice = newprice.replace(r'[^0-9,.]', '')
@@ -1764,8 +1764,8 @@ while jsonprods:
                                 #print("Error when scraping misc. product information for product ID " + product['productid'] + ": " + sys.exc_info()[0] + " occured!")
                                 print(traceback.format_exc())
                         # >>> MAKE PRICES NUMERIC <<< #
-                        if not re.match('[0-9]', price): price = ''
-                        if not re.match('[0-9]', salesprice): salesprice = ''
+                        #if not re.match('[0-9]', price): price = ''
+                        #if not re.match('[0-9]', salesprice): salesprice = ''
                         price = getmoneyfromtext(price)
                         salesprice = getmoneyfromtext(salesprice)
                         # >>> STORE PRODUCT VALUES IN MORPH.IO DATABASE <<< #
