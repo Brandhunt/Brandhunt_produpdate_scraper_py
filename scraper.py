250a89d02419c10e4aa355b784a13ec292ebf330
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-06-19 15:32:27 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1157,6 +1157,26 @@ while jsonprods:
                                                             for cat_remove in cats_to_remove:
                                                                 product_categories = list(filter(lambda x: re.search(cat_remove, x[0]['name'],\
                                                                                                                      flags=re.IGNORECASE), product_categories))
+                                                        if re.search('{add_category}', fix[1], flags=re.IGNORECASE):
+                                                            cats_to_add = re.sub('\{add_category\}', '', fix[1]).split(',')
+                                                            for cat_add in cats_to_add:
+                                                                found_cats = list(filter(lambda x: re.search(cat_add, x[0]['name'],\
+                                                                                                                     flags=re.IGNORECASE), product_categories))
+                                                                if len(found_cats) == 0:
+                                                                    cat_add_name = cat_add.strip()
+                                                                    clean_cat = slugify(cat_add_name.strip())
+                                                                    term = doesprodattrexist(jsonprodattr['product_cat'], cat_add_name, 'product_cat')
+                                                                    if term:
+                                                                        if not list(filter(lambda x: x[0]['term_id'] == term['term_id'], category_array)):
+                                                                            product_categories.append((term, False))
+                                                                            cat_parents = term['ancestors']
+                                                                            for parent_id in cat_parents:
+                                                                                parent = doesprodattrexist(jsonprodattr['product_cat'], parent_id, 'product_cat')
+                                                                                if not list(filter(lambda x: x[0]['term_id'] == parent['term_id'], category_array)):
+                                                                                    category_array.append((parent, False))
+                                                                    else:
+                                                                        term = {'term_id':-1, 'name':cat_add_name, 'slug':clean_cat, 'taxonomy':'product_cat'}
+                                                                        product_categories.append((term, True))        
                                         # --- Get color attributes from current scrape --- #
                                         if productmisc_array[(i-1)] == 'pa_color_html':
                                             colories = jsonprodattr['pa_color']
